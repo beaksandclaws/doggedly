33d8bb7eb3f9373c9951f8b612143cda8c526126
Kellen Hawley <hawleykc@gmail.com>
2015-03-11 19:02:07 -0700

    Annotate everything

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id             :integer          not null, primary key
+#  path           :string(255)
+#  active         :boolean
+#  imageable_type :string(255)
+#  imageable_id   :integer
+#  user_id        :integer
+#  width          :integer
+#  height         :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Photo < ActiveRecord::Base
   belongs_to :user
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text(65535)
+#  rating     :integer
+#  active     :boolean
+#  user_id    :integer
+#  place_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
